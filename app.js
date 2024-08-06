7121f77bdf08f5f2d4cf45b83c529951e022a359
Chen <jonkiky@gmail.com>
2024-08-06 15:12:55 -0400

    remove the mysql part as we will not use it

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const config = require("./Config");
 const logger = require("./Components/logger");
-const mysql = require("./Components/mysql");
 const elasticsearch = require("./Components/elasticsearch");
 
 const app = express();
@@ -16,17 +15,6 @@ const startServer = async () => {
     } else {
       logger.info("Failed to connect to Elasticsearch.");
     }
-
-    // Test MySQL connection
-    if (mysql !== null) {
-      const mysqlConnected = await mysql.query("SELECT 1 AS c1");
-      if (mysqlConnected[0].c1) {
-        logger.info("Relational DB connected!");
-      } else {
-        logger.info("Failed to connect to Relational Database.");
-      }
-    }
-
     // Start server
     app.listen(config.port, () => {
       logger.info("Server listening on %d, in %s mode", config.port, config.env);
@@ -42,8 +30,5 @@ startServer();
 // Handle graceful shutdown
 process.on("SIGINT", () => {
   logger.info("Gracefully shutting down :)");
-  if (mysql && typeof mysql.close === "function") {
-    mysql.close();
-  }
   process.exit();
 });
