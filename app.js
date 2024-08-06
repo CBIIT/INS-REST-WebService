4cec315b88a267b8772d216ae0481ff362278802
Chen <jonkiky@gmail.com>
2024-08-06 14:56:14 -0400

    update the env file:

--- app.js
+++ app.js
@@ -7,47 +7,43 @@ const elasticsearch = require("./Components/elasticsearch");
 const app = express();
 require("./Config/express")(app);
 
-const startServer = async function(){
-  // Setup server
-
-  try{
+const startServer = async () => {
+  try {
+    // Test Elasticsearch connection
     const elasticsearchConnected = await elasticsearch.testConnection();
-    if(elasticsearchConnected){
+    if (elasticsearchConnected) {
       logger.info("Elasticsearch connected!");
-    }
-    else{
+    } else {
       logger.info("Failed to connect to Elasticsearch.");
     }
-  }
-  catch(error) {
-    logger.error(error);
-  }
-  
 
-try{
-  const mysqlConnected = await mysql.query("select 1 as c1");
-  if(mysqlConnected[0].c1){
-    logger.info("Relational DB connected!");
-  }
-  else{
-    logger.info("Failed to connect to Relational Database.");
-  }
-}
-catch(error) {
-  logger.error(error);
-}
+    // Test MySQL connection
+    if (mysql !== null) {
+      const mysqlConnected = await mysql.query("SELECT 1 AS c1");
+      if (mysqlConnected[0].c1) {
+        logger.info("Relational DB connected!");
+      } else {
+        logger.info("Failed to connect to Relational Database.");
+      }
+    }
 
-  // Start server
-  app.listen(config.port, function () {
-    logger.info("Server listening on %d, in %s mode", config.port, config.env);
-  });
+    // Start server
+    app.listen(config.port, () => {
+      logger.info("Server listening on %d, in %s mode", config.port, config.env);
+    });
+  } catch (error) {
+    logger.error("Error starting server:", error);
+  }
 };
 
+// Start the server
 startServer();
 
-// when shutdown signal is received, do graceful shutdown
-process.on("SIGINT", function () {
-	logger.info("Gracefully shutting down :)");
-	mysql.close();
-	process.exit();
+// Handle graceful shutdown
+process.on("SIGINT", () => {
+  logger.info("Gracefully shutting down :)");
+  if (mysql && typeof mysql.close === "function") {
+    mysql.close();
+  }
+  process.exit();
 });
