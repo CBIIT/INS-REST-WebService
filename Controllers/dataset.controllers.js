02d7d2532a8e62af4d7bd309868f3839a2a10004
Joon Lee <joon.lee2@nih.gov>
2024-10-25 20:04:53 -0400

    Changed some spacing and indentation

--- Controllers/dataset.controllers.js
+++ Controllers/dataset.controllers.js
@@ -7,67 +7,72 @@ const datasetFields = require('../Utils/datasetFields');
 const datasetService = require('../Services/dataset.service');
 
 const search = async (req, res) => {
-    const body = req.body;
-    const data = {};
-    const filters = body.filters ?? {};
-    const options = {};
-    const pageInfo = body.pageInfo ?? {page: 1, pageSize: 10};
-    const searchText = body.search_text?.trim() ?? '';
-    const sort = body.sort ?? {k: 'dbGaP_phs', v: 'asc'};
+  const body = req.body;
+  const data = {};
+  const filters = body.filters ?? {};
+  const options = {};
+  const pageInfo = body.pageInfo ?? {page: 1, pageSize: 10};
+  const searchText = body.search_text?.trim() ?? '';
+  const sort = body.sort ?? {k: 'dbGaP_phs', v: 'asc'};
 
-    if (pageInfo.page !== parseInt(pageInfo.page, 10) || pageInfo.page <= 0) {
-      pageInfo.page = 1;
-    }
-    if (pageInfo.pageSize !== parseInt(pageInfo.pageSize, 10) || pageInfo.pageSize <= 0) {
-      pageInfo.pageSize = 10;
-    }
-    // if(sort.k === "primary_dataset_scope") {
-    //   sort.name = "Primary Dataset Scope";
-    //   sort.k = "primary_dataset_scope";
-    // } else if (sort.k === "dataset_name.raw") {
-    //   sort.name = "Dataset";
-    //   sort.k = "dataset_name.raw";
-    // } else if (sort.k === "case_id") {
-    //   sort.name = "Cases";
-    //   sort.k = "case_id";
-    // } else if (sort.k === "sample_id") {
-    //   sort.name = "Samples";
-    //   sort.k = "sample_id";
-    // } else {
-    //   sort.name = "Resource";
-    //   sort.k = "data_resource_id";
-    // }
-    if (!(sort.v && ['asc', 'desc'].includes(sort.v))) {
-      sort.v = 'asc';
-    }
+  if (pageInfo.page !== parseInt(pageInfo.page, 10) || pageInfo.page <= 0) {
+    pageInfo.page = 1;
+  }
+  if (pageInfo.pageSize !== parseInt(pageInfo.pageSize, 10) || pageInfo.pageSize <= 0) {
+    pageInfo.pageSize = 10;
+  }
+  // if(sort.k === "primary_dataset_scope") {
+  //   sort.name = "Primary Dataset Scope";
+  //   sort.k = "primary_dataset_scope";
+  // } else if (sort.k === "dataset_name.raw") {
+  //   sort.name = "Dataset";
+  //   sort.k = "dataset_name.raw";
+  // } else if (sort.k === "case_id") {
+  //   sort.name = "Cases";
+  //   sort.k = "case_id";
+  // } else if (sort.k === "sample_id") {
+  //   sort.name = "Samples";
+  //   sort.k = "sample_id";
+  // } else {
+  //   sort.name = "Resource";
+  //   sort.k = "data_resource_id";
+  // }
+  if (!(sort.v && ['asc', 'desc'].includes(sort.v))) {
+    sort.v = 'asc';
+  }
 
-    options.pageInfo = pageInfo;
-    options.sort = sort;
-    const searchResult = await datasetService.search(searchText, filters, options);
-    if (searchResult.total !== 0 && (options.pageInfo.page - 1) * options.pageInfo.pageSize >= searchResult.total) {
-      let lastPage = Math.ceil(searchResult.total / options.pageInfo.pageSize);
-      options.pageInfo.page = lastPage;
-      const searchResultAgain = await datasetService.search(searchText, filters, options);
-      data.pageInfo.total = searchResultAgain.total;
-      data.result = searchResultAgain.data;
-      data.aggs = searchResultAgain.aggs;
-    } else {
-      data.pageInfo.total = searchResult.total;
-      data.result = searchResult.data;
-      data.aggs = searchResult.aggs;
-    }
+  options.pageInfo = pageInfo;
+  options.sort = sort;
+  const searchResult = await datasetService.search(searchText, filters, options);
+  if (searchResult.total !== 0 && (options.pageInfo.page - 1) * options.pageInfo.pageSize >= searchResult.total) {
+    let lastPage = Math.ceil(searchResult.total / options.pageInfo.pageSize);
+    options.pageInfo.page = lastPage;
+    const searchResultAgain = await datasetService.search(searchText, filters, options);
+    data.pageInfo.total = searchResultAgain.total;
+    data.result = searchResultAgain.data;
+    data.aggs = searchResultAgain.aggs;
+  } else {
+    data.pageInfo.total = searchResult.total;
+    data.result = searchResult.data;
+    data.aggs = searchResult.aggs;
+  }
 
-    data.sort = sort;
-    data.pageInfo = options.pageInfo;
-    res.json({status:"success", data: data});
+  data.sort = sort;
+  data.pageInfo = options.pageInfo;
+  res.json({status:"success", data: data});
 };
 
 const export2CSV = async (req, res) => {
   const body = req.body;
-  const searchText = body.search_text?.trim() ?? "";
+  const csvFields = Object.entries(datasetFields).map(([naturalName, propertyName]) => ({
+    label: naturalName,
+    value: propertyName,
+  }));
   const filters = body.filters ?? {};
+  const options = {};
   const pageInfo = {page: 1, pageSize: 5000};
-  const sort = body.sort ?? {k: "dbGaP_phs", v: "asc"};
+  const searchText = body.search_text?.trim() ?? '';
+  const sort = body.sort ?? {k: 'dbGaP_phs', v: 'asc'};
 
   if (pageInfo.page !== parseInt(pageInfo.page, 10) || pageInfo.page <= 0) {
     pageInfo.page = 1;
@@ -91,53 +96,49 @@ const export2CSV = async (req, res) => {
   //   sort.name = "Resource";
   //   sort.k = "data_resource_id";
   // }
-  if (!(sort.v && ["asc", "desc"].includes(sort.v))) {
-    sort.v = "asc";
+  if (!(sort.v && ['asc', 'desc'].includes(sort.v))) {
+    sort.v = 'asc';
   }
-  let options = {};
+
   options.pageInfo = pageInfo;
   options.sort = sort;
   const searchResult = await datasetService.export2CSV(searchText, filters, options);
-  const csvFields = Object.entries(datasetFields).map(([naturalName, propertyName]) => ({
-    label: naturalName,
-    value: propertyName,
-  }));
   const json2Csv = new Parser({ csvFields });
   const csv = json2Csv.parse(searchResult);
   res.header('Content-Type', 'text/csv');
-  res.attachment("export.csv");
+  res.attachment('export.csv');
   res.send(csv);
 };
 
 const getById = async (req, res) => {
   const datasetId = req.params.datasetId;
   const searchResult = await datasetService.searchById(datasetId);
-  res.json({status:"success", data: searchResult});
+  res.json({status:'success', data: searchResult});
 };
 
 const getFilters = async (req, res) => {
   const body = req.body;
-  const searchText = body.search_text?.trim() ?? "";
+  const searchText = body.search_text?.trim() ?? '';
   const searchFilters = body.filters ?? {};
 
   const filters = await datasetService.getFilters(searchText, searchFilters);
 
-  res.json({status: "success", data: filters});
+  res.json({status: 'success', data: filters});
 };
 
 const getAdvancedFilters = async (req, res) => {
   let advancedFilters = await datasetService.getAdvancedFilters();
-  res.json({status: "success", data: advancedFilters});
+  res.json({status: 'success', data: advancedFilters});
 };
 
 const getDatasetCount = async (req, res) => {
   let options = {};
   let filters = [];
-  let searchText = "";
+  let searchText = '';
   options.pageInfo = {page: 1, pageSize: 10};
-  options.sort = {k: "dbGaP_phs", v: "asc"};
+  options.sort = {k: 'dbGaP_phs', v: 'asc'};
   const searchResult = await datasetService.search(searchText, filters, options);
-  res.json({status: "success", data: searchResult.total});
+  res.json({status: 'success', data: searchResult.total});
 }
 
 module.exports = {
--- Services/dataset.service.js
+++ Services/dataset.service.js
@@ -1,10 +1,10 @@
-const config = require("../Config");
-const elasticsearch = require("../Components/elasticsearch");
-const cache = require("../Components/cache");
-const mysql = require("../Components/mysql");
-const queryGenerator = require("./queryGenerator");
-const cacheKeyGenerator = require("./cacheKeyGenerator");
-const utils = require("../Utils");
+const config = require('../Config');
+const elasticsearch = require('../Components/elasticsearch');
+const cache = require('../Components/cache');
+const mysql = require('../Components/mysql');
+const queryGenerator = require('./queryGenerator');
+const cacheKeyGenerator = require('./cacheKeyGenerator');
+const utils = require('../Utils');
 
 const search = async (searchText, filters, options) => {
   let result = {};
@@ -12,7 +12,7 @@ const search = async (searchText, filters, options) => {
   if (false && searchableText !== "") {
     let aggregationKey = cacheKeyGenerator.getAggregationKey(searchableText);
     let aggregation = cache.getValue(aggregationKey);
-    if(!aggregation){
+    if (!aggregation) {
       let query = queryGenerator.getSearchAggregationQuery(searchText);
       let searchResults = await elasticsearch.searchWithAggregations(config.indexDS, query);
       aggregation = searchResults.aggs.myAgg.buckets;
@@ -20,15 +20,15 @@ const search = async (searchText, filters, options) => {
       cache.setValue(aggregationKey, aggregation, config.itemTTL/288);
     }
     const aggs = aggregation.map((agg) => agg.key);
-    result.aggs = aggs.join("|");
+    result.aggs = aggs.join('|');
   } else {
-    result.aggs = "all";
+    result.aggs = 'all';
   }
   
   let query = queryGenerator.getSearchQueryV2(searchText, filters, options);
   let searchResults = await elasticsearch.searchWithAggregations(config.indexDS, query);
   let datasets = searchResults.hits.hits.map((ds) => {
-    if(ds.inner_hits) {
+    if (ds.inner_hits) {
       const terms = Object.keys(ds.inner_hits);
       const additionalHitsDict = {};
       if (terms.length > 0) {
@@ -39,7 +39,7 @@ const search = async (searchText, filters, options) => {
               additionalHitsDict[hit._nested.offset].source = hit._source;
               additionalHitsDict[hit._nested.offset].highlight = [];
             }
-            additionalHitsDict[hit._nested.offset].highlight = additionalHitsDict[hit._nested.offset].highlight.concat(hit.highlight["additional.attr_set.k"]);
+            additionalHitsDict[hit._nested.offset].highlight = additionalHitsDict[hit._nested.offset].highlight.concat(hit.highlight['additional.attr_set.k']);
           });
         });
       }
@@ -48,7 +48,7 @@ const search = async (searchText, filters, options) => {
         const tmp = {};
         tmp.content = additionalHitsDict[key].source;
         tmp.highlight = {};
-        tmp.highlight["additional.attr_set.k"] = utils.consolidateHighlight(additionalHitsDict[key].highlight);
+        tmp.highlight['additional.attr_set.k'] = utils.consolidateHighlight(additionalHitsDict[key].highlight);
         additionalHits.push(tmp);
       }
       return {content: ds._source, highlight: ds.highlight, additionalHits: additionalHits};
@@ -71,7 +71,7 @@ const export2CSV = async (searchText, filters, options) => {
 const searchById = async (id) => {
   let datasetKey = cacheKeyGenerator.datasetKey(id);
   let dataset = cache.getValue(datasetKey);
-  if(!dataset){
+  if (!dataset) {
     let query = queryGenerator.getDatasetByIdQuery(id);
     let searchResults = await elasticsearch.search(config.indexDS, query);
     let datasets = searchResults.hits.map((ds) => {
@@ -115,26 +115,26 @@ const getFilters = async (searchText, searchFilters) => {
 const getAdvancedFilters = async () => {
   let advancedFiltersKey = cacheKeyGenerator.advancedFiltersKey();
   let advancedFilters = cache.getValue(advancedFiltersKey);
-  if(!advancedFilters){
+  if (!advancedFilters) {
     //querying elasticsearch, save to dataresources cache
     //let sql = "select lt.term_name as name, lvs.permissible_value as value from lu_terms lt, lu_value_set lvs where lt.id = lvs.term_id and lt.term_name in (?,?,?,?,?,?,?,?,?,?,?,?)";
-    let sql = "select data_element, element_value, dataset_count from aggragation where data_element in (?,?,?,?,?,?,?,?,?,?,?,?,?)";
+    let sql = 'select data_element, element_value, dataset_count from aggragation where data_element in (?,?,?,?,?,?,?,?,?,?,?,?,?)';
 
     let inserts = [
-      "Case Disease Diagnosis",
-      "Sample Is Cell Line",
-      "Case Tumor Site",
-      "Case Treatment Administered",
-      "Case Treatment Outcome",
-      "Sample Anatomic Site",
-      "Sample Assay Method",
-      "Sample Analyte Type",
-      "Sample Composition Type",
-      "Sample Is Normal",
-      "Case Age at Diagnosis",
-      "Case Ethnicity",
-      "Case Race",
-      "Case Sex"
+      'Case Disease Diagnosis',
+      'Sample Is Cell Line',
+      'Case Tumor Site',
+      'Case Treatment Administered',
+      'Case Treatment Outcome',
+      'Sample Anatomic Site',
+      'Sample Assay Method',
+      'Sample Analyte Type',
+      'Sample Composition Type',
+      'Sample Is Normal',
+      'Case Age at Diagnosis',
+      'Case Ethnicity',
+      'Case Race',
+      'Case Sex'
     ];
     sql = mysql.format(sql, inserts);
     const result = await mysql.query(sql);
@@ -148,21 +148,21 @@ const getAdvancedFilters = async () => {
         advancedFilters[kv.data_element].push(kv.element_value);
       });
       //add case count
-      advancedFilters["Number of Cases"] = [
-        "0 - 10 Cases",
-        "10 - 100 Cases",
-        "100 - 1000 Cases",
-        "> 1000 Cases",
+      advancedFilters['Number of Cases'] = [
+        '0 - 10 Cases',
+        '10 - 100 Cases',
+        '100 - 1000 Cases',
+        '> 1000 Cases',
       ];
       //add sample count
-      advancedFilters["Number of Samples"] = [
-        "0 - 10 Samples",
-        "10 - 100 Samples",
-        "100 - 1000 Samples",
-        "> 1000 Samples",
+      advancedFilters['Number of Samples'] = [
+        '0 - 10 Samples',
+        '10 - 100 Samples',
+        '100 - 1000 Samples',
+        '> 1000 Samples',
       ];
       //sort and top n
-      for(let k in advancedFilters){
+      for (let k in advancedFilters) {
         const tmp = advancedFilters[k];
         tmp.sort();
         //advancedFilters[k] = tmp.length > config.limitAdvancedFilterCount ? tmp.slice(0, config.limitAdvancedFilterCount) : tmp;
