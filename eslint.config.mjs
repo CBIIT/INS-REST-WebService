e9d09377e4442c0e23630d3ab56c16f7a9f978b0
Alec M. <alec.mattu@nih.gov>
2026-05-15 10:18:39 -0400

    chore: Update ESLint configuration and add eslint-plugin-import-x

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,10 +3,29 @@ import globals from "globals";
 import markdown from "@eslint/markdown";
 import { defineConfig } from "eslint/config";
 import prettier from "eslint-plugin-prettier/recommended";
+import { importX } from "eslint-plugin-import-x";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "module" } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js, "import-x": importX },
+    extends: ["js/recommended", "import-x/flat/recommended"],
+    languageOptions: { globals: globals.node },
+    rules: {
+      "no-unreachable": "error",
+      "eol-last": "error",
+      "no-console": ["error", { allow: ["warn", "error"] }],
+
+      "import-x/newline-after-import": ["error", { count: 1, considerComments: true }],
+      "import-x/no-extraneous-dependencies": [
+        "error",
+        {
+          devDependencies: ["**/*.{test}.{js,ts}", "**/setupTests.{js,ts}", "vite.config.js", "eslint.config.mjs"],
+          optionalDependencies: false,
+        },
+      ],
+    },
+  },
   { files: ["**/*.md"], plugins: { markdown }, language: "markdown/gfm", extends: ["markdown/recommended"] },
-  prettier
+  prettier,
 ]);
